938f85752a9131eb52a0ce0237b8501c33e34831
Ramazan <ospanr4@gmail.com>
2024-11-10 20:55:43 +0500

    change env

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -138,8 +138,8 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'ramazanospan@yandex.kz'
-EMAIL_HOST_PASSWORD = 'pmrbgfqcybgpklyo'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGGING = {
